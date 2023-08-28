13562638b512c9fab09d647a271bd30b9178c2eb
Pedro Dagostini <87324617+Dagostini01@users.noreply.github.com>
2023-08-28 17:58:02 -0300

    html criado

---
+++ src/main/java/br/com/sprint/app/controller/TaskController.java
@@ -0,0 +1,12 @@
+package br.com.sprint.app.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TaskController {
+
+    @GetMapping("/create")
+    public String create() {
+        return "create";
+    }
+}
