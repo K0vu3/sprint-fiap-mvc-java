aaa2412333b2a73caf98fab4996f9770048afd1b
Pedro Dagostini <87324617+Dagostini01@users.noreply.github.com>
2023-09-09 21:51:06 -0300

    produto post criado

--- src/main/java/br/com/sprint/app/controller/ProdutoController.java
+++ src/main/java/br/com/sprint/app/controller/ProdutoController.java
@@ -1,12 +1,27 @@
 package br.com.sprint.app.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import br.com.sprint.app.model.Produto;
 
 @Controller
 public class ProdutoController {
 
-    @GetMapping("/create")
-    public String create() {
+    List<Produto> produtos = new ArrayList<>();
+
+    @GetMapping("/home")
+    public String home() {
         return "create";
+    }
+
+    @PostMapping("/create")
+    public void create(Produto produto) {
+        Long id = produtos.size() + 1L;
+        produtos.add(new Produto(id, produto.getNome(), produto.getDate()));
     }
 }
