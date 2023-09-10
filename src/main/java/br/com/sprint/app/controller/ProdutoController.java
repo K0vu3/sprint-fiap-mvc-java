9d519418df7ecd2522ffe70a73777c7bbb4faa59
Pedro Dagostini <87324617+Dagostini01@users.noreply.github.com>
2023-09-10 15:57:46 -0300

    edit controller

--- src/main/java/br/com/sprint/app/controller/ProdutoController.java
+++ src/main/java/br/com/sprint/app/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -32,6 +33,16 @@ public class ProdutoController {
     public ModelAndView list() {
         ModelAndView mv = new ModelAndView("list");
         mv.addObject("produtos", produtos);
+        return mv;
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView edit(@PathVariable("id") Long id) {
+        ModelAndView mv = new ModelAndView("create");
+
+        Produto produtoFind = produtos.stream().filter(produto -> id.equals(produto.getId())).findFirst().get();
+        mv.addObject("produto", produtoFind);
+
         return mv;
     }
 
